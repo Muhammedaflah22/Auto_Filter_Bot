827cb3423188c3ca38b8efb4d595c5112de469ec
Muhammedaflah22 <botzzaflahbotzz@gmail.com>
2025-10-25 12:10:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Adarsh:Adarsh@cluster0.uv6azie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://carlosadarusa_db_user:BTM1453@cluster0.sfsiijx.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
